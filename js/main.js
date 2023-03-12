cba5a7c93bd384e85596e9bf718d006972458ede
AnmolPlayzz <74979911+AnmolPlayzz@users.noreply.github.com>
2023-03-12 22:18:45 +0530

    beta-0.2 - About page, awesome locomotive scroll animations and a nice cursor tracker. ~ !! NO MOBILE SUPPORT AS OF NOW !!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+const cursor = new MouseFollower({
+    container: 'main',
+    speed: 0.3
+});
+
 const anchorTags = document.querySelectorAll('.navbar__desktop__link a');
 anchorTags.forEach(anchor => {
     anchor.addEventListener('click', function(event) {
@@ -7,7 +12,26 @@ anchorTags.forEach(anchor => {
     });
 });
 
-const scroller = new LocomotiveScroll({
+
+const scroll = new LocomotiveScroll({
     el: document.querySelector('.main__viewport'),
-    smooth: true
+    smooth: true,
+    mobile: {
+        smooth: true
+    },
+    tablet: {
+        smooth: true
+    }
 });
+
+function trans() {
+    document.querySelector('.transition div:nth-child(1)').style.height='50vh'
+}
+
+scroll.on('call', (func, args, obj) => {
+    //this[func]();
+    //console.log(args);
+    //console.log(obj.id);
+    console.log("a")
+});
+
